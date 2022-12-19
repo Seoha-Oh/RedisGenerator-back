5328e41f5de9cc51239738273653e2b27aa15a9e
seoha <seoha8952@gmail.com>
2022-12-19 16:57:49 +0900

    package 구성

---
+++ src/main/java/com/seoha/RedisGenerator/RedisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.seoha.RedisGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seoha/RedisGenerator/RedisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoha.RedisGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
